d553e4f822591796abca946e1d034059e16a15d2
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-08 00:18:46 -0500

    removing activites

--- mysite/src/Activities/Activities.js
+++ mysite/src/Activities/Activities.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import "./Activities.css";
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import {Carousel} from "react-bootstrap";
-import borderImage from "./borderImage.png";
+
 function Activities()
 {
-    const useStyles = makeStyles({
-        root: {
-          maxWidth: 345,
-        },
-      });
-
-      const classes = useStyles();
-    return (
-        <div class="">
-           
-  
-        </div>
-    )
+    
 }
 
 export default Activities;
