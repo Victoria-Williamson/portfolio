644c18ced9311bf6d8ac82774c10c22f853add09
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-20 19:52:12 -0500

    cleaning up about code

--- mysite/src/AboutMe/AboutMe.js
+++ mysite/src/AboutMe/AboutMe.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./AboutMe.css";
 import Self from "./images/Self.svg"
 import Grid from '@material-ui/core/Grid';
+import { data } from "./AboutMe_Info";
 function AboutMe()
 {
     return (
@@ -33,30 +34,21 @@ function AboutMe()
                     </div>
                 </div>
 </div>
-  <div class="h-auto  flex-wrap flex grid mt-5 grid-rows-2 items-center justify-center sm:grid-flow-row grid-flow-col gap-2 text-center">
-  <div class="text-gradient font-extrabold text-white text-3xl font-bold"> Languages <div>
+  <div class="h-auto grid mt-5 grid-rows-2 items-center justify-center sm:grid-flow-row grid-flow-col gap-2 text-center">
+  <div class="text-gradient font-bold text-white text-3xl"> Languages <div>
       </div> <div class="bg-gradient-to-r from-darkPink via-darkOrange to-darkGreen  mt-1 h-3  w-72">  </div> </div>
 </div>
 <div class="h-auto ">
 <div class="flex flex-wrap text-center justify-center px-10 lg:px-64">
-<div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6" src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"/>
+{data.languages.map((lang) =>
+{
+       return(
+       <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0 text-center align-middle">
+       
+         <img  class="w-5/6 h-5/6 flex items-center justify-center object-scale-down px-1"  alt={lang.title} src={lang.image}/>
   </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6"  src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"/>
-  </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6"  src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"/>
-  </div>
-  <div class="flex-col items-center text-center">
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 p-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/1200px-Javascript-shield.svg.png"/>
-  </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 p-2 object-scale-down" src="https://cdn.freebiesupply.com/logos/large/2x/java-4-logo-png-transparent.png"/>
-  </div>
- 
-  </div>
+       )
+})} 
 </div>
 </div>
 <div class="h-auto mt-5  lg:px-64 flex-wrap flex grid grid-rows-2 items-center justify-center sm:grid-flow-row grid-flow-col gap-2 text-center">
@@ -66,30 +58,14 @@ function AboutMe()
 </div>
 <div class="h-auto ">
 <div class="flex flex-wrap text-center justify-center px-10">
-<div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6" src="https://cdn.worldvectorlogo.com/logos/tailwindcss.svg"/>
-  </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 object-scale-down p-2"  src="https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png"/>
-  </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 object-scale-down"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/115px-Tensorflow_logo.svg.png"/>
-  </div>
-  <div class="flex-col items-center text-center">
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 p-2 object-scale-down" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/260px-Scikit_learn_logo_small.svg.png"/>
-  </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 p-2 object-scale-down" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Pandas_mark.svg/1200px-Pandas_mark.svg.png"/>
-  </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 p-2 object-scale-down" src="https://user-images.githubusercontent.com/50221806/86498201-a8bd8680-bd39-11ea-9d08-66b610a8dc01.png"/>
-  </div>
-  <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
-         <img  class="w-5/6 h-5/6 p-2 object-scale-down" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
-  </div>
- 
-  </div>
+{data.frameworks.map((framework) =>
+{
+       return(
+              <div class="lg:w-32 lg:h-32 h-20 w-20 m-2 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
+              <img  class="w-5/6 h-5/6" alt={framework.title} src={framework.image}/>
+       </div>
+       )
+})} 
 </div>
 </div>
 
@@ -101,77 +77,50 @@ function AboutMe()
 <div class="mx-8 my-4">
 <h2 class="font-medium title-font tracking-widest text-lightPink mb-4 text-lg font-bold text-center sm:text-left mt-3">Statistics and Math Courses</h2>
 <li class="text-white list-none text-lg">
+{data.math.map((course) =>
+{
+       return(
 <ul>
             <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
             <svg class="fill-current text-darkPink" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
              
-            </span> Statistics 1 and 2
+            </span> {course}
     </ul>
-          <ul>
-          <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-            <svg class="fill-current text-darkPink" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-             
-            </span>Calculus 1,2, and 3
-          </ul>
-          <ul>
-          <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-            <svg class="fill-current text-darkPink" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-             
-            </span> Matrix and Linear Algebra
-          </ul>
+       )
+})} 
 </li>
 </div>
-<div class="mx-8 my-4">
+<div class="mx-8">
 <h2 class="font-medium title-font tracking-widest text-lightOrange mb-4 text-lg font-bold text-center sm:text-left text-darkPink mt-3">General Computer Science</h2>
 <li class="text-white list-none text-center text-lg">
+{data.compSci.map((course) =>
+{
+       return(
 <ul>
             <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
             <svg class="fill-current text-darkOrange" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
              
-            </span> C Programming
+            </span> {course}
     </ul>
-    <ul>
-            <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-            <svg class="fill-current text-darkOrange" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-             
-            </span> Java
-    </ul>
-    <ul>
-            <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-            <svg class="fill-current text-darkOrange" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-             
-            </span> Data Structures
-    </ul>
-    <ul>
-            <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-            <svg class="fill-current text-darkOrange" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-             
-            </span> Algorithm Analysis
-    </ul>
-         
+       )
+})} 
 </li>
 </div>
-<div class="mx-8 mt-4">
+<div class="mx-8">
 <h2 class="font-medium title-font tracking-widest  mb-4 text-lg font-bold text-center sm:text-left text-darkGreen mt-3">Electives</h2>
 <li class="text-white list-none text-lg ">
+{data.electives.map((course) =>
+{
+
+       return(
 <ul>
             <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
             <svg class="fill-current text-darkGreen" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
              
-            </span> Artificial Intelligence
+            </span> {course}
     </ul>
-    <ul>
-            <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-            <svg class="fill-current text-darkGreen" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-             
-            </span> Robot Vision
-    </ul>
-    <ul>
-            <span class=" text-2xl w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
-            <svg class="fill-current text-darkGreen" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-             
-            </span> Algorithms For Machine Learning
-    </ul>
+       )
+})} 
 </li>
 </div>
 </div>
---
+++ mysite/src/AboutMe/AboutMe_Info.js
@@ -0,0 +1,83 @@
+export const data = 
+{
+    languages:
+    [
+        {
+            title: 'HTML',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg'
+        },
+        {
+            title: 'CSS',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
+        },
+        {
+            title:'Python',
+            image:'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg'
+        },
+        {
+            title: 'JavaScript',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/1200px-Javascript-shield.svg.png',
+        },
+        {
+            title: 'Java',
+            image: 'https://cdn.freebiesupply.com/logos/large/2x/java-4-logo-png-transparent.png',
+        },
+        {
+            title: 'TypeScript',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png',
+        }
+    ],
+
+    frameworks:
+    [
+        {
+            title: 'TailwindCSS',
+            image: 'https://cdn.worldvectorlogo.com/logos/tailwindcss.svg'
+        },
+        {
+            title: 'SciKit Learn',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/260px-Scikit_learn_logo_small.svg.png'
+        },
+        {
+            title: 'Pandas',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Pandas_mark.svg/1200px-Pandas_mark.svg.png'
+        },
+        {
+            title: 'Numpy',
+            image:'ttps://user-images.githubusercontent.com/50221806/86498201-a8bd8680-bd39-11ea-9d08-66b610a8dc01.png',
+        },
+        {
+            title: 'React',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+        },
+        {
+            title: 'MongoDB',
+            image: 'https://store-images.s-microsoft.com/image/apps.12235.d167ffaf-d6b9-41aa-b07c-a5e72ec09aa0.f6657533-9443-481d-b53c-b45afdfdfe53.80296830-3a2d-4fed-8b9d-f090c14aaebb'
+        },
+    ],
+
+    math:
+    [
+        "Statistics 1 and 2",
+        "Calculus 1,2, and 3",
+        "Matrix and Linear Algebra",
+        "Statistical Theory 1",
+        "Computational Methods for Applied Statistics",
+        "Statistical Foundations of Data Science and Artificial Intelligence"
+    ],
+    compSci:
+    [
+        "C Programming",
+        "Java",
+        "Data Structures",
+        "Algorithm Analysis",
+        "Processes for Object Oriented Software Development"
+    ],
+    electives:
+    [
+        "Artificial Intelligence",
+        "Robot Vision",
+        "Algorithms for Machine Learning",
+        "Robotic Systems",
+    ]
+}
